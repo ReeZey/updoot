66c8256a5a6d71e84d8bd8133ed90f90f50a0e3f
ReeZey <Johannes@seved.net>
2024-09-30 02:56:17 +0200

    its a bad idea to read everything to RAM

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::fs;
+use std::{fs::File, io::Write};
 
 use byte_unit::Byte;
-use config::{Config, File, FileFormat};
+use config::{Config, FileFormat};
 use mws::{html::Status, utils::{format_response, format_response_with_body}, WebServer};
 use once_cell::sync::Lazy;
-use tokio::{io::AsyncWriteExt, sync::Mutex};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, sync::Mutex};
 
 static CONFIG: Lazy<Mutex<Config>> = Lazy::new(|| {
     Mutex::new(
         Config::builder()
-            .add_source(File::new("config.toml", FileFormat::Toml))
+            .add_source(config::File::new("config.toml", FileFormat::Toml))
             .build().unwrap()
     )
 });
@@ -20,8 +20,8 @@ async fn main() {
     let port = config.get_int("port").unwrap() as u16;
     drop(config);
 
-    let server = WebServer::new(true);
-    server.listen("0.0.0.0", port, |mut request| async move {
+    let server = WebServer::new("0.0.0.0", port, true);
+    server.listen(|mut request| async move {
         let config = CONFIG.lock().await;
         let server_key = config.get_string("secret_key").expect("secret_key not found");
         let types = config.get_table("type").expect("expected atleast one path");
@@ -39,14 +39,6 @@ async fn main() {
                 return;
             }
         };
-
-        let options = types.get(file_type).unwrap().clone().into_table().unwrap();
-        let upload_limit = Byte::parse_str(options.get("limit").unwrap().clone().into_string().unwrap(), true).unwrap().as_u64() as usize;
-
-        if request.body.len() > upload_limit {
-            request.stream.write_all(&format_response(Status::PayloadTooLarge)).await.unwrap();
-            return;
-        }
 
         match request.headers.get("secret-key") {
             Some(key) => {
@@ -69,8 +61,30 @@ async fn main() {
             }
         };
 
-        println!("{} just uploaded {} with size of {:?}", request.get_real_ip(None), file_name, request.body.len());
-        fs::write(format!("{}/{}", options.get("path").unwrap().clone().into_string().unwrap(), file_name), request.body).unwrap();
+
+        let options = types.get(file_type).unwrap().clone().into_table().unwrap();
+        let upload_limit = Byte::parse_str(options.get("limit").unwrap().clone().into_string().unwrap(), true).unwrap().as_u64() as usize;
+
+        let total_size = request.get_header("content-length").unwrap().parse::<usize>().unwrap();
+
+        if total_size > upload_limit {
+            request.stream.write_all(&format_response(Status::PayloadTooLarge)).await.unwrap();
+            return;
+        }
+
+        let file = format!("{}/{}", options.get("path").unwrap().clone().into_string().unwrap(), file_name);
+        let mut file = File::create(file).unwrap();
+
+        let mut bytes_read = 0;
+        while bytes_read < total_size {
+            let mut buffer = vec![0; 1024];
+            let bytes = request.stream.read(&mut buffer).await.unwrap();
+            
+            file.write_all(&buffer[..bytes]).unwrap();
+            bytes_read += bytes;
+        }
+
+        println!("{} just uploaded {} with size of {:?}", request.get_real_ip(None), file_name, total_size);
 
         let text = urlencoding::encode(file_name).to_string();
         request.stream.write_all(&format_response_with_body(Status::OK, text)).await.unwrap();
